e4d48d44674bf546b5cdb6b688efab2ecba2dfda
fiedsch <fiedsch@ja-eh.at>
2016-10-22 10:29:59 +0200

    remove most calendar functionality for regular backend users
    this is done as regular users don’t have to use these buttons and it
    makes life easier for them. Admins still have the Contao standards

--- contao/system/modules/harlekin/dca/tl_calendar_events.php
+++ contao/system/modules/harlekin/dca/tl_calendar_events.php
@@ -68,3 +68,17 @@ $GLOBALS['TL_DCA']['tl_calendar_events']['fields']['url']['search'] = false;
 // Ein Event soll standardmäßig veröffentlicht sein
 $GLOBALS['TL_DCA']['tl_calendar_events']['fields']['published']['sql']
     = str_replace("default ''", "default '1'", $GLOBALS['TL_DCA']['tl_calendar_events']['fields']['published']['sql']);
+
+/**
+ * hide some (almost all) buttons if user is not admin as
+ * regular Users are only supposed to edit calendar_events.
+ */
+if (!\BackendUser::getInstance()->isAdmin) {
+    foreach (['edit', 'copy', /*'delete',*/'show','toggle','cut'] as $operation) {
+        unset($GLOBALS['TL_DCA']['tl_calendar_events']['list']['operations'][$operation]);
+    }
+    // change editheader icon to edit icon which is familar to the user
+    $GLOBALS['TL_DCA']['tl_calendar_events']['list']['operations']['editheader']['icon'] = 'edit.gif';
+    // remove the all-operation which does not work anymore after our unset(...) above
+    unset($GLOBALS['TL_DCA']['tl_calendar_events']['list']['global_operations']['all']);
+}
