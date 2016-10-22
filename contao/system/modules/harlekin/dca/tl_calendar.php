9cabf5fbaa4726a2689cd272e8c42ac4f1382a58
fiedsch <fiedsch@ja-eh.at>
2016-10-22 10:29:59 +0200

    remove most calendar functionality for regular backend users
    this is done as regular users don’t have to use these buttons and it
    makes life easier for them. Admins still have the Contao standards

--- contao/system/modules/harlekin/dca/tl_calendar.php
+++ contao/system/modules/harlekin/dca/tl_calendar.php
@@ -1,4 +1,13 @@
 <?php
-
-
+/**
+ * hide some (almost all) buttons if user is not admin as
+ * regular Users are only supposed to edit calendar_events.
+ */
+if (!\BackendUser::getInstance()->isAdmin) {
+    foreach (['editheader', 'copy','delete','show'] as $operation) {
+        unset($GLOBALS['TL_DCA']['tl_calendar']['list']['operations'][$operation]);
+    }
+    // remove the all-operation which does not work anymore after our unset(...) above
+    unset($GLOBALS['TL_DCA']['tl_calendar']['list']['global_operations']['all']);
+}
 
