8065ec8b2a3ea35a787e2d159817aa70c00309e9
Andreas Fieger <fiedsch@ja-eh.at>
2019-05-20 07:14:23 +0200

    Delete install.sh
    Datei gelöscht, die nur auf dem lokalen Entwicklungsserver benötigt wird

--- contao/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SITE=/Users/andreas/Sites/harlekin
-
-EXTENSION=harlekin
-
-cp -r ./system/modules/$EXTENSION $SITE/system/modules
-sudo chown -R _www:staff $SITE
-sudo chmod -R  ug+rw $SITE
